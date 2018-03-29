fcf09368edc36b9db4db7445bbe6fa671b3dcef8
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-29 19:16:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }else{
     cart= [];
-    tot = total();
+    var tot = total();
     return `Your total cost is $${tot}, which will be charged to the card ${cardNumber}.`
   }
 }
